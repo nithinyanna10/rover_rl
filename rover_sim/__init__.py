9a88a0e1b68b58a4d7ad2344f628b7559ab24cc5
Nithin Yanna <nithinyanna10@users.noreply.github.com>
2026-02-18 20:53:47 -0500

    Update rover_sim/__init__.py: doc geometry_utils and map_generator

--- rover_sim/__init__.py
+++ rover_sim/__init__.py
@@ -7,6 +7,8 @@ Components:
 - sensors: LiDAR and related sensor models
 - render: pygame-based visualization
 - env: Gymnasium-compatible RL environment
+- geometry_utils: angle/point/segment/polygon helpers
+- map_generator: procedural map generation (corridors, clutter, rooms, maze)
 """
 
 from .world import World, Obstacle
